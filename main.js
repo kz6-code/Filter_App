718c6d62e56a1fdb24b72bbc0fbb640680fe96b6
kz6-code <80061504+kz6-code@users.noreply.github.com>
2022-02-17 18:18:34 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+loop = true
+
+function preload() {
+}
+while (loop == true){
+  canvas.center
+}
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot(){    
+  save('Test.png');
+}
